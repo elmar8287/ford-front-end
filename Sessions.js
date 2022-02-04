7a848fec9d43d91848b61e3e1f7c6d4aa220127f
Elmar <L-mar@inbox.ru>
2022-02-04 10:01:59 +0400

    Create connected status

--- Sessions.js
+++ Sessions.js
@@ -1,4 +1,29 @@
+import axios from 'axios';
+
 // eslint-disable-next-line import/prefer-default-export
+export const isConnect = async () => {
+  let answer = false;
+  let token = '';
+  if (localStorage.getItem('token') != null) {
+    token = localStorage.getItem('token');
+  }
+  await axios.get('http://carhubackend.herokuapp.com/member-data', {
+    headers: {
+      authorization: token,
+    },
+  })
+    .then((response) => response)
+    .then((response) => {
+      if (response.data.message === "If you see this, you're in!") {
+        answer = true;
+      } else {
+        localStorage.setItem('token', '');
+      }
+      return answer;
+    })
+    .catch((error) => error);
+  return answer;
+};
 
 export const goBack = () => {
   isConnect().then((data) => {
